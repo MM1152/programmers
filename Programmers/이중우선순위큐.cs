7fcc2d396a319169884555616dc3b82ceb471a2c
Computer <albert6306@naver.com>
2024-06-04 22:40:17 +0900

    3번 테케 실패

--- Programmers/이중우선순위큐.cs
+++ Programmers/이중우선순위큐.cs
@@ -1,10 +1,21 @@
 using System;
+using Microsoft.VisualBasic;
 
 public class main{
     public static void Main(string[] args){
         Solution solution = new Solution();
-        string[] word = {"I 100", "I 90", "I 80", "D 1", "D 1", "I 95", "I 105", "D -1", "D -1"};
-        Console.WriteLine( solution.solution(word)[0]);
+        string[] word = {"D 1",
+"D -1",
+"D -1",
+"I 8088",
+"D 1",
+"I 5585",
+"I 9097",
+"D 1",
+"D -1",
+    };
+        int[] answer =  solution.solution(word);
+        Console.WriteLine(answer[0] +" " + answer[1]);
     }
 }
 
@@ -18,27 +29,29 @@ public class Solution {
             string[] word = a.Split(" ");
             
             if(word[0].Equals("I")){
-                Console.WriteLine($"Add : {word[1]}");
                 minHeap.add(int.Parse(word[1]));
+                maxHeap.MaxAdd(int.Parse(word[1]));
             }
-
-           
 
             else if(word[0].Equals("D")){
                 if(word[1].Equals("-1") && minHeap.getHeap().Count > 1){
-                    minHeap.remove();
+                    int value = minHeap.removeMin();
+                    maxHeap.removeValue(value);
                 }else if(word[1].Equals("1") && minHeap.getHeap().Count > 1){
-                    minHeap.removeMax();
+                    int value = maxHeap.removeMax();
+                    minHeap.removeValue(value);
                 }
             }
-            foreach(var b in minHeap.getHeap()){
-                Console.Write($"{b} -> ");
-            }
         }
-        
-        Console.WriteLine();
-        answer[0] = -maxHeap.getHeap()[0];
+        foreach(var a in minHeap.getHeap()){
+            Console.Write($"{a} -> ");
+        }
+        if(maxHeap.getHeap().Count == 1){
+            return answer;
+        }
+        answer[0] = maxHeap.getHeap()[1];
         answer[1] = minHeap.getHeap()[1];
+        Console.WriteLine();
         return answer;
     }
 }
@@ -48,10 +61,26 @@ public class Solution {
         public Heap(){
             heap.Add(0);
         }
-
         public void add(int value){
             heap.Add(value);
             sort();
+        }
+        public void MaxAdd(int value){
+            heap.Add(value);
+            MaxSort();
+        }
+        public void MaxSort(){
+            int point = heap.Count - 1;
+            while(point / 2 != 0){
+                int parent = point / 2;
+                if(heap[parent] < heap[point]){
+                    Swap(parent , point);
+                }
+                else {
+                    break;
+                }
+                point = parent;
+            }
         }
         public void sort(){
             int point = heap.Count - 1;
@@ -66,12 +95,32 @@ public class Solution {
                 point = parent;
             }
         }
-        public void removeMax(){
-            Console.WriteLine($"RemoveMax {heap[heap.Count - 1]}");
-            heap.RemoveAt(heap.Count - 1);
+        public int removeMax(){
+            int point = heap.Count - 1;
+            int returnValue = heap[1];
+            heap[1] = heap[point];
+            heap.RemoveAt(point);
+            point = 1;
+            while(point < heap.Count){
+                int left = point * 2;
+                int right = point * 2 + 1;
+                int next = point;
+                if(left < heap.Count && heap[left] > heap[point]) next = left;
+                if(right < heap.Count && heap[right] > heap[point]) next = right;
+                
+                if(next == point){
+                    break;
+                }
+                
+                Swap(point , next);
+            }
+            return returnValue;
         }
-        public void remove(){
-            Console.WriteLine($"RemoveMin {heap[1]}");
+        public void removeValue(int value){
+            heap.Remove(value);
+        }
+        public int removeMin(){
+            int returnValue = heap[1];
             int point = heap.Count - 1;
             heap[1] = heap[point];
             heap.RemoveAt(point);
@@ -89,6 +138,7 @@ public class Solution {
                 
                 Swap(point , next);
             }
+            return returnValue;
         }
         public void Swap(int parent , int swapPoint){
             int temp = heap[parent];
